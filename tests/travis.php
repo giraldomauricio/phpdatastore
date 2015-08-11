7685581a14434405a56bc807255349f5cbfc7b09
Mauricio Giraldo <mgiraldo@gmail.com>
2015-08-11 17:59:19 -0400

    Updated for Travis

--- tests/travis.php
+++ tests/travis.php
@@ -4,4 +4,4 @@ include_once('AutoLoader.php');
 // Register the directory to your include files
 AutoLoader::registerDirectory('./lib/');
 //AutoLoader::registerDirectory('./PHPUnit/Framework/');
-define("DS_ROOT","/home/travis/build/giraldomauricio/phpdatastore/tests/temp/");
+define("DS_ROOT","../tests/temp/");
